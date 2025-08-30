c0e4e4cbc076187893a7d4296a8741f70d628a74
worldOfVerdure <perspicaciousdotard@gmail.com>
2025-08-30 10:52:43 -0500

    Set up About Me section logic (wip)

--- src/components/AboutME/AboutMe.tsx
+++ src/components/AboutME/AboutMe.tsx
@@ -1,25 +1,40 @@
-// import React from 'react';
-// import { Stack, Box, Typography} from '@mui/material';
+import Portrait from './Portrait.tsx';
+import { Stack, Typography} from '@mui/material';
 
-// const AboutMe = () => {
-//   return (
-//     <Stack
-//       alignItems="center"
-//       component="section"
-//       direction={{xs: 'column', md: 'row'}}
-//       spacing={4}
-//        sx={{
-//         px: { xs: 2, md: 4 },
-//         py: { xs: 4, md: 8 },
-//       }}
-//     >
-//       {/* Heading */}
-//       <Typography
-//         sx={{ flexShrink: 0, textAlign: { xs: 'center', md: 'left' } }}
-//         variant="h1"
-//       >
-//         {title}
-//       </Typography>
-//     </Stack>
-//   );
-// }
+const AboutMe = () => {
+  return (
+    <Stack
+      alignItems="center"
+      component="section"
+      direction={{xs: 'column', md: 'row'}}
+      spacing={4}
+       sx={{
+        px: { xs: 2, md: 4 },
+        py: { xs: 4, md: 8 },
+      }}
+    >
+      {/* Heading */}
+      <Typography
+        sx={{ flexShrink: 0, textAlign: { xs: 'center', md: 'left' } }}
+        variant="h1"
+      >
+        {title} {/* Need title here */}
+      </Typography>
+      <Stack>
+        {/* Image */}
+      <Box 
+        alt={imageAlt}
+        component="img"
+        sizes=" "
+        src= " "
+        srcSet=" "
+        sx={{
+          width: { xs: '100%', md: '40%' },
+          borderRadius: 2,
+          objectFit: 'fill'
+        }}
+      />
+      </Stack>
+    </Stack>
+  );
+}
